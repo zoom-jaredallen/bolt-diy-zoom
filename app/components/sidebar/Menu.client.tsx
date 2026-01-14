d16e317132c80f754a3c9de991b6feedaf668eae
Jared Allen <jared.allen@zoom.us>
2026-01-15 10:59:56 +1100

    Minor changes

--- app/components/sidebar/Menu.client.tsx
+++ app/components/sidebar/Menu.client.tsx
@@ -1,4 +1,4 @@
-pimport { motion, type Variants } from 'framer-motion';
+import { motion, type Variants } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Dialog, DialogButton, DialogDescription, DialogRoot, DialogTitle } from '~/components/ui/Dialog';
